9de0024a554f70dbc1d85163f2799ad0c08d4ae5
Bulkan Evcimen <bulkan@gmail.com>
2011-09-01 16:43:42 +1000

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from os.path import abspath, dirname, join
+execfile(join(dirname(abspath(__file__)), 'src', 'rf-requestslibrary', 'version.py'))
+
+DESCRIPTION = """
+"""[1:-1]
+
+
+CLASSIFIERS = """
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
+
+setup(name         = 'robotframework-requests',
+      version      = VERSION,
+      description  = 'Robot Framework keyword library wrapper around requests',
+      long_description = DESCRIPTION,
+      author       = 'Bulkan Savun Evcimen',
+      author_email = 'bulkan@gmail.com',
+      url          = 'http://github.com/bulkan/robotframework-requests',
+      license      = 'Apache License 2.0',
+      keywords     = 'robotframework testing test automation http client requests',
+      platforms    = 'any',
+      classifiers  = CLASSIFIERS.splitlines(),
+      package_dir  = {'' : 'src'},
+      packages     = ['RequestsLibrary'],
+      )
