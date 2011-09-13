f1b3288099d24ad048abe0905eea963bd949034e
Bulkan Evcimen <bulkan@gmail.com>
2011-09-13 13:54:29 +1000

    Updated the example testcase in doc string

--- src/RequestsLibrary/__init__.py
+++ src/RequestsLibrary/__init__.py
@@ -10,12 +10,12 @@ class RequestsLibrary(RequestsKeywords):
 
         Examples:
         | Create Session | google | http://www.google.com |
-        | Create Session  github  | http://github.com/api/v2/json |
-        | ${resp}= | Get  google  |  /|
-        | Should Be Equal As String s |  ${resp.status_code} | 200 |
-        | ${resp}= | Get  github  | /user/search/bulkan|
+        | Create Session | github  | http://github.com/api/v2/json |
+        | ${resp} | Get  google  |  / |
+        | Should Be Equal As Strings |  ${resp.status_code} | 200 |
+        | ${resp} | Get  github  | /user/search/bulkan |
         | Should Be Equal As Strings  |  ${resp.status_code} | 200 |
-        | ${jsondata}=  | To Json |  ${resp.content}|
+        | ${jsondata}  | To Json |  ${resp.content} |
         | Dictionary Should Contain Value | ${jsondata['users'][0]} | Bulkan Savun Evcimen |
 
     """
