b70df43fece9b9eaae3f4e9b101443bcad15ce8b
Luca Giovenzana <luca@giovenzana.org>
2021-12-03 16:28:02 +0100

    fix(doc): restore decorators after doc generation

--- doc/generate_doc.sh
+++ doc/generate_doc.sh
@@ -4,4 +4,5 @@ SCRIPT_PATH=`dirname "$0"`
 sed -i.bak '/@warn_if_equal_symbol_in_url_session_less/d' ${SCRIPT_PATH}/../src/RequestsLibrary/RequestsKeywords.py
 sed -i.bak '/@warn_if_equal_symbol_in_url_on_session/d' ${SCRIPT_PATH}/../src/RequestsLibrary/RequestsOnSessionKeywords.py
 python -m robot.libdoc ${SCRIPT_PATH}/../src/RequestsLibrary ${SCRIPT_PATH}/RequestsLibrary.html
+mv ${SCRIPT_PATH}/../src/RequestsLibrary/RequestsKeywords.py.bak ${SCRIPT_PATH}/../src/RequestsLibrary/RequestsKeywords.py
 mv ${SCRIPT_PATH}/../src/RequestsLibrary/RequestsOnSessionKeywords.py.bak ${SCRIPT_PATH}/../src/RequestsLibrary/RequestsOnSessionKeywords.py
